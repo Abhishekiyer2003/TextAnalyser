91646379858325fb28dab248dac93f921384a64e
Abhishek balaji <Abhishekiyer2003@gmail.com>
2026-06-21 19:42:26 +0530

    Added the SearchText function

--- TextAnalyser.py
+++ TextAnalyser.py
@@ -1,21 +1,28 @@
 import re
-inputfile=open("Demoanalyse")
+import os
+FileName="Demoanalyse"
 def displaycontent():
-    inputfile=open("Demoanalyse")
-    print(inputfile.read())
+    print(FileName.read())
 def SearchText(text):
-
-    with inputfile as f:
+    with open(FileName) as f:
         for line_no,files in enumerate(f,start=1):
             if re.search(text, files):
                 return f"Found the Text at {line_no}"
 def Addtext(text):
-    inputfile=open("Demoanalyse","a")
-    with  inputfile as f:
+    with  open(FileName,"a") as f:
         f.write(text)
         return f"Written"
-def UpdateText(new_text):
-    inputfile=open("Demoanalyse","w")
+def DeleteText(text):
+    with open(FileName, "r") as f:
+        content = f.read()
+
+    # Remove all occurrences of the word
+    new_content = re.sub(text, "", content)
+
+    with open(FileName, "w") as f:
+        f.write(new_content)
+
+    return "Text deleted successfully"
 
 
 
@@ -32,5 +39,6 @@ userText=input("Enter a text you want to search")
 print(SearchText(userText))
 text=input("Enter a text to be written")
 print(Addtext(text))
+print(DeleteText(text))
 
 
